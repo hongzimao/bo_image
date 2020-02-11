8f57259c59a8a36e3b06a63d661614a9862d85fd
Hongzi Mao <hongzi@mit.edu>
2020-02-11 15:37:41 -0500

    set up data window

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gsp
+from matplotlib.widgets import Slider
 from matplotlib.widgets import Button
 from img.interact import sliders
 from img.basic import load_image
@@ -36,7 +37,7 @@ def main():
                    'sharpness', 'saturation', 'vibrance']
     slider_count = len(adjustments)
     slider_grid = gsp.GridSpecFromSubplotSpec(
-        slider_count, 1, gs[6:, 8:12])
+        slider_count, 1, gs[8:, 8:12])
     slider_axes = []
     for (i, text) in enumerate(adjustments):
         slider_ax = fig.add_subplot(slider_grid[i, 0])
@@ -59,12 +60,34 @@ def main():
                  slider_vmins, slider_vmaxs,
                  slider_vinit)
 
-    # resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-    # button = Button(resetax, 'Reset', color='lightgoldenrodyellow', hovercolor='0.975')
+    # add a slider for value
+    slider_ax = fig.add_subplot(gsp.GridSpecFromSubplotSpec(
+        1, 1, gs[5, 8:12])[0, 0], facecolor='lightgoldenrodyellow')
+    score_slider = Slider(slider_ax, 'Score', 0, 10, 5)
 
-    # def reset(event):
-    #     ss[0].set_val(8)
-    # button.on_clicked(reset)
+    # plot the score history
+    scores = []
+    line_ax = fig.add_subplot(gs[0:3, 8:12])
+    score_obj, = line_ax.plot(scores)
+    line_ax.grid(linestyle=(0, (10, 10)), color='black', linewidth=0.2)
+    line_ax.set_ylim([-1, 11])
+    line_ax.set_xlabel('Steps')
+    line_ax.set_ylabel('Scores')
+
+    # add next button
+    button_ax = fig.add_subplot(gsp.GridSpecFromSubplotSpec(
+        1, 1, gs[6, 8:12])[0, 0])
+    button = Button(button_ax, 'Bayesian Optimization',
+        color='lightgoldenrodyellow', hovercolor='0.975')
+
+    # button performs BO
+    def next(event):
+        scores.append(score_slider.val)
+        score_obj.set_data(np.arange(len(scores)), scores)
+        line_ax.set_xlim([-1, len(scores)])
+        fig.canvas.draw_idle()
+        score_slider.set_val(5)
+    button.on_clicked(next)
 
     plt.show()
 
